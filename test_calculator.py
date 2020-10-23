27107586bf48b7e3ce26b7abdf20b29220413f10
Vyshakh <vyshakh.m@broadcom.com>
2020-10-23 14:38:14 +0530

    "Add unit tests for calculator"

---
+++ test_calculator.py
@@ -0,0 +1,14 @@
+"""
+    Unit tests for calculator library
+"""
+
+import calculator
+
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == calculator.add(2,2)
+
+    def test_substraction(self):
+        assert 2 == calculator.substract(4,2)
