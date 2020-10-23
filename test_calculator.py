9f06a196deaf4bf0103b4f2784156850714a6bbe
Vyshakh <vyshakh.m@broadcom.com>
2020-10-23 15:24:53 +0530

    "linter fix"

--- test_calculator.py
+++ test_calculator.py
@@ -8,7 +8,7 @@ import calculator
 class TestCalculator:
 
     def test_addition(self):
-        assert 4 == calculator.add(2,2)
+        assert 4 == calculator.add(2, 2)
 
     def test_substraction(self):
-        assert 2 == calculator.substract(4,2)
+        assert 2 == calculator.substract(4, 2)
