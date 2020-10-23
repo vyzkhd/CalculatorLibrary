dec6e12a3383805e7528ba2dd919b31bdb659d4e
Vyshakh <vyshakh.m@broadcom.com>
2020-10-23 15:12:34 +0530

    "final"

--- calculator.py
+++ calculator.py
@@ -2,8 +2,10 @@
     Calculator library containing basic math operations.
 """
 
+
 def add(first_term, second_term):
     return first_term + second_term
+
 
 def substract(first_term, second_term):
     return first_term - second_term
