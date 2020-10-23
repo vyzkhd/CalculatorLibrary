510225fa551513d098ac8a406c04cbd49e0842a6
Vyshakh <vyshakh.m@broadcom.com>
2020-10-23 15:40:30 +0530

    "add multiplication"

--- test_calculator.py
+++ test_calculator.py
@@ -12,3 +12,7 @@ class TestCalculator:
 
     def test_substraction(self):
         assert 2 == calculator.substract(4, 2)
+
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10, 10)
+
